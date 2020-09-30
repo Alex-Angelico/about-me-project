97f2e6d185157e8f8440a868d3832d3d072ecc32
Alex Angelico <alex.angelico@live.com>
2020-09-30 14:23:27 -0700

    added README, debugging number guess code block

--- javascript/app.js
+++ javascript/app.js
@@ -72,6 +72,7 @@
 var number = 5;
 var guess = Number(prompt('Surprise! The quiz isn\'t over, there\'s two more questions to go. But let\'s try something new...\n\nWhat number am I thinking of between 1 and 10? I\'ll give you four guesses.'));
 console.log(typeof guess);
+console.log(typeof number);1
 for (var i = 0; i < 3; i++) {
     if (i = 0 && guess === number) {
         answerCount++;
@@ -85,16 +86,53 @@ for (var i = 0; i < 3; i++) {
         guess = prompt('Too low. Try again.');
     } else if (i <= 3 && guess > number) {
         guess = prompt('Too high. Try again.');
-    } else if (i === 4) {
+    } else if (i > 3) {
         alert('That\'s enough guesses! The answer was 5. Your score is still ' + answerCount + '/7. Now it\'s time for the real last question...');
         break;
-    } else {
-        alert('That\'s not a number, but it still counts as a guess!')
-        if (i <= 3) {
-            guess = prompt('Try again.');
-        } else {
-            alert('That\'s enough guesses! The answer was 5. Your score is still ' + answerCount + '/7. Now it\'s time for the real last question...');
-            break;
-        }
+    // } else {
+    //     alert('That\'s not a number, but it still counts as a guess!')
+    //     if (i <= 3) {
+    //         guess = prompt('Try again.');
+    //     } else {
+    //         alert('That\'s enough guesses! The answer was 5. Your score is still ' + answerCount + '/7. Now it\'s time for the real last question...');
+    //         break;
+    //     }
     }
 }
+
+// function numberGuess() {
+//     var number = "72";
+//     var guess = prompt("That's what I like to hear!\n\nOh, by the way, I've got a game to play while you're out hiking with a buddy: guess the number. Play between 1 and a 1,000,000 and see how many miles go by before one of you guesses--or pushes the other down a switchback! Before you move on to check out the trails, let's try a practice run...\n\nWhat number am I thinking of between 1 and 100?");
+//     for (var i = 1; i < 10; i++) {
+//         if (i < 3 && guess === number) {
+//             alert("That was fast!");
+//             break;
+//         } else if (i < 3 && guess !== number) {
+//             guess = prompt("Nope. Guess again.");
+//         } else {
+//             if ((i >= 3 && i < 6) && guess === number) {
+//                 alert("There you go.");
+//                 break;
+//             } else if ((i >= 3 && i < 6) && guess !== number) {
+//                 if (i === 3) {
+//                     guess = prompt("I'll give you a hint: It's a number bigger than 50.");
+//                 } else {
+//                     guess = prompt("Nope. Guess again.");
+//                 }
+//             } else {
+//                 if ((i >= 6 && i < 9) && guess === number) {
+//                     alert("Whew! That took a while.");
+//                     break;
+//                 } else if ((i >= 6 && i < 9) && guess !== number) {
+//                     if (i === 6) {
+//                         guess = prompt("Alright, one more hint: It's a number between than 70 and 79.");
+//                     } else {
+//                         guess = prompt("Nope. Guess again.");
+//                     }
+//                 } else if (i === 9 && guess !== number) {
+//                     alert("Fine, I'll just let you through. The number was 72.");
+//                 }
+//             }
+//         }
+//     }
+// }
