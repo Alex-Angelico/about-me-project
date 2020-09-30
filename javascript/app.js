c7c79c7635647ce57333cff49896a631e5504034
Alex Angelico <alex.angelico@live.com>
2020-09-30 14:09:27 -0700

    modifying number guess

--- javascript/app.js
+++ javascript/app.js
@@ -1,76 +1,76 @@
 'use strict'
 
-var userName = prompt('Welcome to my page. What\'s your name?');
-alert('Hi ' + userName + '. Let\'s start off with a fun 5-question game of Y/N trivia--about yours truly, Alex!');
+// var userName = prompt('Welcome to my page. What\'s your name?');
+// alert('Hi ' + userName + '. Let\'s start off with a fun 5-question game of Y/N trivia--about yours truly, Alex!');
 
-var answerCount = 0;
+// var answerCount = 0;
 
-var loveChocolate = prompt('Y/N: Alex loves chocolate.').toLowerCase();
-if (loveChocolate === 'y' || loveChocolate === 'yes') {
-    // console.log("Q1 Correct");
-    answerCount++;
-    alert('Correct! Science has yet to prove I can survive without it. You now have ' + answerCount + '/5 correct answers. Next question...');
-} else if (loveChocolate === 'n' || loveChocolate === 'no') {
-    alert('Incorrect! That\'s ' + answerCount + '/5 correct answers. Next question...');
-} else {
-    answerCount = 0;
-    alert('You have dared insult the rules of Y/N trivia! No points for you, back to 0! Ahem, next question...')
-}
+// var loveChocolate = prompt('Y/N: Alex loves chocolate.').toLowerCase();
+// if (loveChocolate === 'y' || loveChocolate === 'yes') {
+//     // console.log("Q1 Correct");
+//     answerCount++;
+//     alert('Correct! Science has yet to prove I can survive without it. You now have ' + answerCount + '/5 correct answers. Next question...');
+// } else if (loveChocolate === 'n' || loveChocolate === 'no') {
+//     alert('Incorrect! That\'s ' + answerCount + '/5 correct answers. Next question...');
+// } else {
+//     answerCount = 0;
+//     alert('You have dared insult the rules of Y/N trivia! No points for you, back to 0! Ahem, next question...')
+// }
 
-var favYellow = prompt('Y/N: Alex\'s favorite color is yellow.').toLowerCase();
-if (favYellow === 'y' || favYellow === 'yes') {
-    // console.log("Q2 Correct");
-    answerCount++;
-    alert('Correct! Yellow is bright and warm, what\'s not to like? You now have ' + answerCount + '/5 correct answers. Next question...');
-} else if (favYellow === 'n' || favYellow === 'no') {
-    alert('Incorrect! That\'s ' + answerCount + '/5 correct answers. Next question...');
-} else {
-    answerCount = 0;
-    alert('You have dared insult the rules of Y/N trivia! No points for you, back to 0! Ahem, next question...')
-}
+// var favYellow = prompt('Y/N: Alex\'s favorite color is yellow.').toLowerCase();
+// if (favYellow === 'y' || favYellow === 'yes') {
+//     // console.log("Q2 Correct");
+//     answerCount++;
+//     alert('Correct! Yellow is bright and warm, what\'s not to like? You now have ' + answerCount + '/5 correct answers. Next question...');
+// } else if (favYellow === 'n' || favYellow === 'no') {
+//     alert('Incorrect! That\'s ' + answerCount + '/5 correct answers. Next question...');
+// } else {
+//     answerCount = 0;
+//     alert('You have dared insult the rules of Y/N trivia! No points for you, back to 0! Ahem, next question...')
+// }
 
-var ghostBelief = prompt('Y/N: Alex believes in ghosts.').toLowerCase();
-if (ghostBelief === 'y' || ghostBelief === 'yes') {
-    alert('Incorrect! That\'s ' + answerCount + '/5 correct answers. Next question...');
-} else if (ghostBelief === 'n' || ghostBelief === 'no') {
-    // console.log("Q3 Correct");
-    answerCount++;
-    alert('Correct! Boooooo, say I! You now have ' + answerCount + '/5 correct answers. Next question...');
-} else {
-    answerCount = 0;
-    alert('You have dared insult the rules of Y/N trivia! No points for you, back to 0! Ahem, next question...')
-}
+// var ghostBelief = prompt('Y/N: Alex believes in ghosts.').toLowerCase();
+// if (ghostBelief === 'y' || ghostBelief === 'yes') {
+//     alert('Incorrect! That\'s ' + answerCount + '/5 correct answers. Next question...');
+// } else if (ghostBelief === 'n' || ghostBelief === 'no') {
+//     // console.log("Q3 Correct");
+//     answerCount++;
+//     alert('Correct! Boooooo, say I! You now have ' + answerCount + '/5 correct answers. Next question...');
+// } else {
+//     answerCount = 0;
+//     alert('You have dared insult the rules of Y/N trivia! No points for you, back to 0! Ahem, next question...')
+// }
 
-var indianFood = prompt('Y/N: Alex dislikes Indian food.').toLowerCase();
-if (indianFood === 'y' || indianFood === 'yes') {
-    alert('Incorrect! That\'s ' + answerCount + '/5 correct answers. Next question...');
-} else if (indianFood === 'n' || indianFood === 'no') {
-    // console.log("Q4 Correct");
-    answerCount++;
-    alert('Correct! I LOVE Indian food. You now have ' + answerCount + '/5 correct answers. Let\'s get on to the last question...');
-} else {
-    answerCount = 0;
-    alert('You have dared insult the rules of Y/N trivia! No points for you, back to 0! May you redeem yourself with the last question...')
-}
+// var indianFood = prompt('Y/N: Alex dislikes Indian food.').toLowerCase();
+// if (indianFood === 'y' || indianFood === 'yes') {
+//     alert('Incorrect! That\'s ' + answerCount + '/5 correct answers. Next question...');
+// } else if (indianFood === 'n' || indianFood === 'no') {
+//     // console.log("Q4 Correct");
+//     answerCount++;
+//     alert('Correct! I LOVE Indian food. You now have ' + answerCount + '/5 correct answers. Let\'s get on to the last question...');
+// } else {
+//     answerCount = 0;
+//     alert('You have dared insult the rules of Y/N trivia! No points for you, back to 0! May you redeem yourself with the last question...')
+// }
 
-var awesomeQuiz = prompt('We\'re going to get meta with this one.\nY/N: This was an awesome quiz about Alex.').toLowerCase();
-if (awesomeQuiz === 'y' || awesomeQuiz === 'yes') {
-    // console.log("Q5 Correct");
-    answerCount++;
-    alert('Aw, shucks! I\'m glad you think so. I would have given you the point either way on this one, but for what it\'s worth, you ended with ' + answerCount + '/5 correct answers. Enjoy the web page, ' + userName + '.');
-} else if (awesomeQuiz === 'n' || awesomeQuiz === 'no') {
-    // console.log("Q5 Correct");
-    answerCount++;
-    alert('Oh drat. Back to the drawing board. That\'s still worth a point to you though. You\'re finishing with ' + answerCount + '/5 correct answers. Have a good day, ' + userName + '!');
-} else {
-    answerCount = 0;
-    alert('Such insolence towards the rules of Y/N trivia! Here at the very end, of all places! That\'s 0/5 for you. Begone, ' + userName + '!')
-}
+// var awesomeQuiz = prompt('We\'re going to get meta with this one.\nY/N: This was an awesome quiz about Alex.').toLowerCase();
+// if (awesomeQuiz === 'y' || awesomeQuiz === 'yes') {
+//     // console.log("Q5 Correct");
+//     answerCount++;
+//     alert('Aw, shucks! I\'m glad you think so. I would have given you the point either way on this one, but for what it\'s worth, you ended with ' + answerCount + '/5 correct answers. Enjoy the web page, ' + userName + '.');
+// } else if (awesomeQuiz === 'n' || awesomeQuiz === 'no') {
+//     // console.log("Q5 Correct");
+//     answerCount++;
+//     alert('Oh drat. Back to the drawing board. That\'s still worth a point to you though. You\'re finishing with ' + answerCount + '/5 correct answers. Have a good day, ' + userName + '!');
+// } else {
+//     answerCount = 0;
+//     alert('Such insolence towards the rules of Y/N trivia! Here at the very end, of all places! That\'s 0/5 for you. Begone, ' + userName + '!')
+// }
 
-alert('...');
+// alert('...');
 
-var number = "5";
-var guess = prompt('Surprise! The quiz isn\'t over, there\'s two more questions to go. But let\'s try something new...\n\nWhat number am I thinking of between 1 and 10? I\'ll give you four guesses.').Number();
+var number = 5;
+var guess = Number(prompt('Surprise! The quiz isn\'t over, there\'s two more questions to go. But let\'s try something new...\n\nWhat number am I thinking of between 1 and 10? I\'ll give you four guesses.'));
 console.log(typeof guess);
 for (var i = 0; i < 3; i++) {
     if (i = 0 && guess === number) {
