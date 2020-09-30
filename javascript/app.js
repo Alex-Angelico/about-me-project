e19145714fc1bf866806759dc51a4d879e07d59a
Alex Angelico <alex.angelico@live.com>
2020-09-30 13:54:26 -0700

    added number guessing game code block

--- javascript/app.js
+++ javascript/app.js
@@ -66,3 +66,35 @@ if (awesomeQuiz === 'y' || awesomeQuiz === 'yes') {
     answerCount = 0;
     alert('Such insolence towards the rules of Y/N trivia! Here at the very end, of all places! That\'s 0/5 for you. Begone, ' + userName + '!')
 }
+
+alert('...');
+
+var number = "5";
+var guess = prompt('Surprise! The quiz isn\'t over, there\'s two more questions to go. But let\'s try something new...\n\nWhat number am I thinking of between 1 and 10? I\'ll give you four guesses.').Number();
+console.log(typeof guess);
+for (var i = 0; i < 3; i++) {
+    if (i = 0 && guess === number) {
+        answerCount++;
+        alert('Wow, talk about lucky! That\'s ' + answerCount + '/7 correct answers now. Alright, onto the last question, for real this time...');
+        break;
+    } else if ((i > 0 && i <= 3) && guess === number) {
+        answerCount++;
+        alert('Correct! Nice job, you now have ' + answerCount + '/7 right. Now let\'s move onto the actual last question...');
+        break;
+    } else if (i <= 3 && guess < number) {
+        guess = prompt('Too low. Try again.');
+    } else if (i <= 3 && guess > number) {
+        guess = prompt('Too high. Try again.');
+    } else if (i === 4) {
+        alert('That\'s enough guesses! The answer was 5. Your score is still ' + answerCount + '/7. Now it\'s time for the real last question...');
+        break;
+    } else {
+        alert('That\'s not a number, but it still counts as a guess!')
+        if (i <= 3) {
+            guess = prompt('Try again.');
+        } else {
+            alert('That\'s enough guesses! The answer was 5. Your score is still ' + answerCount + '/7. Now it\'s time for the real last question...');
+            break;
+        }
+    }
+}
